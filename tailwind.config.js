945d15ec019b08f395f3df3251e62c255ecb4912
lakshmn778180 <lk842569@gmail.com>
2025-01-06 19:52:44 +0530

    to create the lading page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#336178', // Name it as you like
+      },
+    },
+  },
+  plugins: [],
+};
+
